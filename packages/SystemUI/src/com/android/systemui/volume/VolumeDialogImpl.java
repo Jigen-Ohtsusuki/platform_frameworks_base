11e99fed7f2eb07bcdae64f00981d9f5d190e120
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-20 08:29:23 +0800

    [AxionUI] Volume panel layout improvements
    Change-Id: Ic10e36e2c19dbc23e4fa135135872e59270d220d
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -266,7 +266,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     private ImageButton mRingerIcon;
     private ViewGroup mODICaptionsView;
     private CaptionsToggleImageButton mODICaptionsIcon;
-    private View mSettingsView;
+    private ViewGroup mSettingsView;
     private ImageButton mSettingsIcon;
     private View mExpandRowsView;
     private ExpandableIndicator mExpandRows;
@@ -788,6 +788,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             setGravity(mDialogRowsViewContainer, Gravity.LEFT);
 
             setGravity(mODICaptionsView, Gravity.LEFT);
+            
+            setGravity(mSettingsView, Gravity.LEFT);
 
             mExpandRows.setRotation(-90);
         }
