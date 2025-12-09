matrixx-beta/android_frameworks_base

base: Add the ability to generate key pairs for keybox spoofing The code is used from :- https://github.com/5ec1cff/TrickyStore
Needs :-
HELLBOY017/external_bouncycastle@f70ea01

This allows us to enjoy spoofing keyboxes the right way even with broken TEE.

Change-Id: I3d1fa3163ea6f3380abc922bb041b5b56c09a5d3
Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
Signed-off-by: Kshitij Koyande <koyande72@gmail.com>

keystore/java/android/security/keystore2/AndroidKeyStoreKeyPairGeneratorSpi.java
@@ -54,6 +54,8 @@ import android.text.TextUtils;
 import android.util.ArraySet;
 import android.util.Log;
 
+import com.android.internal.util.crdroid.KeyboxImitationHooks;
+
 import libcore.util.EmptyArray;
 
 import java.math.BigInteger;
@@ -695,8 +697,38 @@ public abstract class AndroidKeyStoreKeyPairGeneratorSpi extends KeyPairGenerato
         try {
             KeyStoreSecurityLevel iSecurityLevel = mKeyStore.getSecurityLevel(securityLevel);
 
-            KeyMetadata metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
-                    constructKeyGenerationArguments(), flags, additionalEntropy);
+            KeyMetadata metadata;
+            if (mSpec.getAttestationChallenge() != null) {
+                KeyboxImitationHooks.setAttestationFlag(true);
+                if (mAttestKeyDescriptor == null) {
+                    KeyboxImitationHooks.setAttestKeyFlag(false);
+                    if ((KeyProperties.KEY_ALGORITHM_EC.equals(mJcaKeyAlgorithm) ||
+                      KeyProperties.KEY_ALGORITHM_RSA.equals(mJcaKeyAlgorithm))) {
+                        metadata = KeyboxImitationHooks.generateKey(
+                            iSecurityLevel.getBinderInterface(),
+                            descriptor,
+                            constructKeyGenerationArguments()
+                        );
+                        if (metadata == null) {
+                            KeyboxImitationHooks.setFailFlag(true);
+                            metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                                    constructKeyGenerationArguments(), flags, additionalEntropy);
+                        }
+                    } else {
+                        metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                                constructKeyGenerationArguments(), flags, additionalEntropy);
+                    }
+                } else {
+                    KeyboxImitationHooks.setAttestKeyFlag(true);
+                    metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                            constructKeyGenerationArguments(), flags, additionalEntropy);
+                }
+            } else {
+                KeyboxImitationHooks.setAttestationFlag(false);
+                KeyboxImitationHooks.setAttestKeyFlag(false);
+                metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                        constructKeyGenerationArguments(), flags, additionalEntropy);
+            }
 
             AndroidKeyStorePublicKey publicKey =
                     AndroidKeyStoreProvider.makeAndroidKeyStorePublicKeyFromKeyEntryResponse(

Jigen-Ohtsusuki/platform_frameworks_base

keystore/java/android/security/keystore2/AndroidKeyStoreKeyPairGeneratorSpi.java
@@ -52,6 +52,8 @@ import android.text.TextUtils;
 import android.util.ArraySet;
 import android.util.Log;
 
+import com.android.internal.util.tequila.KeyboxImitationHooks;
+
 import libcore.util.EmptyArray;
 
 import java.math.BigInteger;
@@ -648,8 +650,38 @@ public abstract class AndroidKeyStoreKeyPairGeneratorSpi extends KeyPairGenerato
         try {
             KeyStoreSecurityLevel iSecurityLevel = mKeyStore.getSecurityLevel(securityLevel);
 
-            KeyMetadata metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
-                    constructKeyGenerationArguments(), flags, additionalEntropy);
+            KeyMetadata metadata;
+            if (mSpec.getAttestationChallenge() != null) {
+                KeyboxImitationHooks.setAttestationFlag(true);
+                if (mAttestKeyDescriptor == null) {
+                    KeyboxImitationHooks.setAttestKeyFlag(false);
+                    if ((KeyProperties.KEY_ALGORITHM_EC.equals(mJcaKeyAlgorithm) ||
+                      KeyProperties.KEY_ALGORITHM_RSA.equals(mJcaKeyAlgorithm))) {
+                        metadata = KeyboxImitationHooks.generateKey(
+                            iSecurityLevel.getBinderInterface(),
+                            descriptor,
+                            constructKeyGenerationArguments()
+                        );
+                        if (metadata == null) {
+                            KeyboxImitationHooks.setFailFlag(true);
+                            metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                                    constructKeyGenerationArguments(), flags, additionalEntropy);
+                        }
+                    } else {
+                        metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                                constructKeyGenerationArguments(), flags, additionalEntropy);
+                    }
+                } else {
+                    KeyboxImitationHooks.setAttestKeyFlag(true);
+                    metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                            constructKeyGenerationArguments(), flags, additionalEntropy);
+                }
+            } else {
+                KeyboxImitationHooks.setAttestationFlag(false);
+                KeyboxImitationHooks.setAttestKeyFlag(false);
+                metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                        constructKeyGenerationArguments(), flags, additionalEntropy);
+            }
 
             AndroidKeyStorePublicKey publicKey =
                     AndroidKeyStoreProvider.makeAndroidKeyStorePublicKeyFromKeyEntryResponse(